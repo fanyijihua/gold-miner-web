b14827b774353bd7c27380fe5f61b37d2d682ea6
Romeo <romeo0906@foxmail.com>
2017-05-07 14:49:20 +0800

    Login feature

--- BackEnd/app/Http/Controllers/Auth/LoginController.php
+++ BackEnd/app/Http/Controllers/Auth/LoginController.php
@@ -4,86 +4,18 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+// use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
-    public function index()
+    public function oAuth()
     {
-        $url = "https://github.com/login/oauth/authorize?client_id=c846cde270a97d98d957&redirect_uri=http://fanyi.juejin.im/auth/getToken&scope=user&state=".$this->randomString();
+        $url = "https://github.com/login/oauth/authorize";
+        $url .= "?client_id=".$this->client_id;
+        $url .= "&state=".$this->randomString(32);
+
         return redirect($url);
     }
 
-    public function randomString()
-    {
-        $range = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        $randomString = '';
-        for($i = 0; $i < 32; $i++){
-            $index = rand(0,strlen($range));
-            $randomString .=substr($range, $index-1, 1);
-        }
-
-        return $randomString;
-    }
-
-    public function getToken(Request $request)
-    {
-        $aUrl = 'https://github.com/login/oauth/access_token';
-        $aParams = array(
-                'state'         =>  $request->input('state'),
-                'code'          =>  $request->input('code'),
-                'client_id'     =>  'c846cde270a97d98d957',
-                'client_secret' =>  'a7b14c8343bded979f1cad9df3964c1ca1047239',
-            );
-
-        $access_token = $this->sendRequest($aUrl, 'POST', $aParams);
-        parse_str($access_token, $ret);
-        
-        $uUrl = "https://api.github.com/user";
-        $uParams = array(
-                'access_token'     =>   $ret['access_token'],
-            );
-        $userInfo = $this->sendRequest($uUrl, 'GET', $uParams);
-
-        var_dump(json_decode($userInfo));
-
-    }
-
-    public function sendRequest($url, $method, $params = array()) 
-    {
-        $options = array(
-                CURLOPT_USERAGENT       =>  'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36',
-                CURLOPT_HTTPHEADER      =>  array(
-                        'Content-type'      =>  'application/x-www-form-urlencoded'                    ),
-                // CURLOPT_HEADER          =>  true,
-                CURLOPT_RETURNTRANSFER  =>  true
-            );
-        
-        $query = http_build_query($params);
-
-        $options[CURLOPT_URL] = $url.($method == 'GET' ? '?'.$query : '');
-
-        if ($method == 'POST') {
-            $options[CURLOPT_POST] = true;
-            $options[CURLOPT_POSTFIELDS] = $query;
-        }
-
-        $sr = curl_init();
-        curl_setopt_array($sr, $options);
-        $ret = curl_exec($sr);
-        curl_close($sr);
-
-        return $ret;
-    }
 }
--- BackEnd/routes/web.php
+++ BackEnd/routes/web.php
@@ -1,22 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('auth/login', 'Auth\LoginController@index');
+Route::get('/auth/login', 'Auth\LoginController@oAuth');
 
-Route::get('auth/getToken', 'Auth\LoginController@getToken');
-
-Route::get('user/info', 'User\UserInfoController@getInfo');
+Route::resource('/api/user', 'Api\UserController');
